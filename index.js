917cc36a6f6ca46174c64cc3445f02ae0a5a8d03
shashikant88093 <shashi94goswami@gmail.com>
2022-12-08 17:12:20 +0530

    added blog home page

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+const PORT = 8000
+const homepage = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+app.use('/',(req, res)=>{
+    res.render("home",{homepage:homepage})
+})
+
+app.listen(PORT,(req, res)=>{
+    console.log(`App is running on port ${PORT}`)
+})
